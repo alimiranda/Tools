8156969060e7d44ff84e472a9bb1d5dff9ed0ea1
Alina Miranda <alina.miranda51@gmail.com>
2022-11-06 12:07:30 -0500

    Se cargan 2 scripts nuevos sobre lectura y escritura en archivos planos. Se actualiza el módulo app aún con fallas :(

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
-import mod
+#import mod
+import utils
+import readcsv
+import charts
 
-data = [
+'''data = [
    {
       'Country' : 'Colombia',
       'Population' : 500
@@ -9,17 +12,25 @@ data = [
       'Country' : 'Bolivia',
       'Population' : 300
    }
-]
+]'''
 
-def run():
+'''def run():
    keys, values = mod.get_population()
    print(keys,values)
-
-
-
    country = input('Type Country = ')
    result = mod.population_by_country(data,country)
-   print(result)
+   print(result)'''
 
-if __name__=='__main__':
-   run()
+def run():
+   data = readcsv.read_csv('/Users/alinasofiamirandamartinez/Documents/1_Datos/world_population.csv')
+   country = input('Type Country => ')
+   
+   result = utils.population_by_country(data, country)
+   if len(result) > 0:
+      country = result[0]
+      labels, values = utils.get_population(country)
+      charts.generate_bar_chart(labels, values)
+
+
+if __name__ == '__main__':
+  run()
---
+++ app/readcsv.py
@@ -0,0 +1,20 @@
+import csv
+
+def read_csv(path):  # creo una función cuyo parámetro d3 entrada es la ruta del archivo que posteriormente leeré
+   with open(path,'r') as csvfile: # Abro el archivo y lo nombro csvfile
+      reader = csv.reader(csvfile,delimiter=',') # creo una variable que lee el archivo separado por comas
+      header = next(reader) # Como reader es un iterable, busco el header de cada columna
+      data=[] # creo una lista vacía donde posteriormente quedaran mis datos organizados por pais
+      for row in reader: # Cada fila corresponde a un país
+         iterable = zip(header,row) # Creo un iterable que une en formato tupla el header y el valor de cada fila en pares
+         print(list(iterable))
+         country_dict = {key:value for key, value in iterable} # Hago un diccionario con las parejas creadas en iteraable donde header es el key y el value es el valor correspondiente en la fila recorrida
+         data.append(country_dict) # Voy agregando cada diccionario a la lista "data"
+      return data # la función retorna la lista de diccionarios con la información por país
+
+
+if __name__=='__main__':
+   read_csv('/Users/alinasofiamirandamartinez/Documents/1_Datos/world_population.csv')
+
+   #print(sys.path)
+   
